56d0da48fd763f4103ac49f483af42e454bbee3f
camhoyos <47069575+camhoyos@users.noreply.github.com>
2021-02-27 10:26:28 -0500

    Add files via upload

---
+++ hamming.h
@@ -0,0 +1,11 @@
+#if !defined(HAMMING_H)
+#define HAMMING_H
+
+#include <string>
+
+namespace hamming {
+    unsigned int compute(const std::string& dna1, const std::string& dna2);
+
+}  // namespace hamming
+
+#endif // HAMMING_H
